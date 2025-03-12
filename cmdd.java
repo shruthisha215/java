372d94f7529a21a793844bcf71fe15441c1955e1
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-12 15:22:52 +0530

    multi commandline program

---
+++ cmdd.java
@@ -0,0 +1,9 @@
+public class cmdd
+{
+    public static void main(String args[])
+    {
+        /*System.out.println("computer science");
+          System.out.println("department");
+        System.out.println("RCET");*/
+          }
+}
