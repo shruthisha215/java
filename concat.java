a09c3977f0010c2a770a912c88ff6dbc5d91f1e4
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-12 14:41:59 +0530

    java program

---
+++ concat.java
@@ -0,0 +1,9 @@
+public class concat
+{
+    public static void main(String args[])
+    {
+        String name1="rohini";
+        String name2="college";
+        System.out.println(name1.concat(name2));  
+          }
+}
