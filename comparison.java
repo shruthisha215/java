e6c1756d6ec1d0786d97d9078f37e8b77ebb22f8
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 11:42:18 +0530

    comparison

---
+++ comparison.java
@@ -0,0 +1,16 @@
+public class comparison
+{
+    public static void main(String args[])
+    {
+        String str1="shruthi";
+        String str2= "sha";
+        if(str1 == str2)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
