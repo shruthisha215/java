0ab13f940a8a8cb81fb22d247ab159c339066c1e
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 13:40:28 +0530

    swapping

---
+++ swap.java
@@ -0,0 +1,15 @@
+public class swap
+{
+    public static void main(String args[])
+    {
+        int a=5,b=6;
+        System.out.println("before swapping: " +a);
+        System.out.println(b);
+        a=(5-5)+6;
+        b=(6-6)+5;
+        System.out.println("after swapping:");
+        System.out.println("a:" +a);
+        System.out.println("b:"+b);
+        
+    }
+}
