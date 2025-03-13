77ce00ab3756ca8dbc99ce998cf826766fbc8773
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 11:47:06 +0530

    comparison optr

---
+++ cmp.java
@@ -0,0 +1,16 @@
+public class cmp
+{
+    public static void main(String args[])
+    {
+        int numb1=1000;
+        int numb2=1500;
+        if(numb1<numb2)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
