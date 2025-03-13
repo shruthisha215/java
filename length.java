5abfd3832459c7f31102225eed5979e8d2d72676
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 14:10:54 +0530

    length of string

---
+++ length.java
@@ -0,0 +1,9 @@
+public class length
+{
+    public static void main(String args[])
+    {
+        String str="WE ARE IN COMPUTER SCIENCE DEPARTMENT";
+        int len=str.length();
+        System.out.println("length of the string is " +len);
+    }
+}
