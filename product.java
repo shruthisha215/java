7d0707ac616788d4236ed22dddda52f8c8d595d4
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 11:06:15 +0530

    multiplication

---
+++ product.java
@@ -0,0 +1,9 @@
+public class product
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        int product=a * b;
+        System.out.println("the product is" + product);
+    }
+}
