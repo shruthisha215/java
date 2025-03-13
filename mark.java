44a9a9bf4fdf521c8d5cdc729f887624f53587f2
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 09:52:47 +0530

    mark

---
+++ mark.java
@@ -0,0 +1,17 @@
+public class mark
+{
+    public static void main(String args[])
+    {
+        String name = "Shruthi";
+        char grade='a';
+        int mark1=90;
+        int mark2=80;
+        int mark3=70;
+        System.out.println("Name of the student:"+ name);
+        System.out.println("Score of physics:"+ mark1);
+        System.out.println("Score of maths: " + mark2);
+        System.out.println("Score of chemistry:"+ mark3);
+        System.out.println("Grade:"+grade);
+
+    }
+}
