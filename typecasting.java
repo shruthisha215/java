90a7d778daa65fa091ec5b30ecb813f5be359080
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 10:22:01 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting
+{
+    public static void main(String args[])
+    {
+        int myint=9;
+        double mydouble = myint;
+        System.out.println(mydouble);
+    }
+}
