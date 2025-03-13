5c85ac5f95cfb8fdb3f5bdeb7b5e8f7b771059a5
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 15:14:39 +0530

    nested statement

---
+++ nested.java
@@ -0,0 +1,21 @@
+public class nested
+{
+    public static void main(String args[])
+    {
+        int mark =90;
+        System.out.println("enter the mark:"+mark);
+        if(mark<=100)
+        {
+            System.out.println("GRADE:O");
+        }
+        else if(mark<=90)
+        {
+            System.out.println("GRADE:A");
+        }
+        else if(mark<=80)
+        {
+            System.out.println("GRADE:B");
+        }
+
+    }
+}
