5b23501c821c63bc14fa36cc9561c93bf9671784
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-13 14:36:14 +0530

    if statement

---
+++ condition.java
@@ -0,0 +1,15 @@
+public class condition
+{
+    public static void main(String args[])
+    {
+        int a=22;
+        if(a>18)
+        {
+            System.out.println("person is eligible to vote");
+        }
+        else
+        {
+            System.out.println("person is illeligible to vote");
+        }
+    }
+}
