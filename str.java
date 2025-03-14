ec30ad68f3fe613526267d3d384da416af3f94bc
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 11:02:22 +0530

    string loop

---
+++ str.java
@@ -0,0 +1,13 @@
+public class str
+{
+   public static void main(String args[])
+   {
+    int i =1;
+    do
+    {
+        System.out.println("CSE");
+        i++;
+    }
+    while(i<=5);
+   }
+}
