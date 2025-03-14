b026f0d3ac2a7b2b7b1d1e2a40f3f4ef0a4c16f2
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 13:14:39 +0530

    method multiplication

---
+++ multi.java
@@ -0,0 +1,14 @@
+public class multi
+{
+    static void mul()
+    {
+        int a =10;
+        int b =20;
+        int c =a*b;
+        System.out.println(c);
+    }
+    public static void main(String args[])
+    {
+        mul();
+    }
+}
