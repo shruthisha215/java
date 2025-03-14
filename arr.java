264d4d63bc0e931c93a476a5266a7a15f2300a02
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 14:48:39 +0530

    dimension arrays

---
+++ arr.java
@@ -0,0 +1,12 @@
+public class arr
+{
+  public static void main(String args[])
+  {
+    String[] str ={"apple","orange","mango","guava","cherry"};
+    str[3]="pineapple";
+    for(int i=0;i<=4;i++)
+    {
+    System.out.println(str[i]);
+      }
+   }
+}
