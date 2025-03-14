baf88a12c63ddf14e2cd8c6bbed2812b6049637c
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 11:34:37 +0530

    decrementation

---
+++ decre.java
@@ -0,0 +1,11 @@
+public class decre
+{
+    public static void main(String args[])
+    {
+        int i=50;
+        for(i=100;i>=10;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
