e0dea55286949475697fbc4aed323749b4825530
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 10:22:40 +0530

    find the string

---
+++ vowel.java
@@ -0,0 +1,16 @@
+public class vowel
+{
+    public static void main(String args[])
+    {
+        String str ="this is an apple";
+        if(str.contains("apple"))
+        {
+            System.out.println("the string contains apple");
+        }
+        else
+        {
+            System.out.println("the string doesnt contains apple");
+        }
+    }
+}    
+        
