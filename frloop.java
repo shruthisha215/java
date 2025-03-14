bf687953079db2eb2ae038d5040893845fe8bc36
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 11:06:27 +0530

    for loop

---
+++ frloop.java
@@ -0,0 +1,11 @@
+public class frloop
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=0;i<=7;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
