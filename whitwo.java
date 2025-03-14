f01dad8239bc4fa6e55a74c1c8912e37bf70bc8e
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 10:37:52 +0530

    while loop

---
+++ whitwo.java
@@ -0,0 +1,12 @@
+public class whitwo
+{
+    public static void main(String args[])
+    {
+        int i =1;
+        while(i<=5)
+        {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
