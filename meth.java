d7dadd61ae7047307537520cda0bfcefa7d25daa
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 13:14:01 +0530

    methods

---
+++ meth.java
@@ -0,0 +1,11 @@
+public class meth
+{
+        static void meth()
+        {
+           System.out.println("WELCOME");
+        }
+    public static void main(String args[])
+    {
+        meth();
+    }
+}
