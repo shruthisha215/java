106e1252b46ae9068efee604f5350dea27aadb55
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 15:44:04 +0530

    class

---
+++ cls.java
@@ -0,0 +1,15 @@
+ class student
+{
+    int id;
+    String name;
+}
+class cls
+{
+    public static void main(String args[])
+    {
+        student s1=new student(); 
+        s1.id=101;
+        System.out.println("the student id is:" +s1.id);
+
+    }
+}
