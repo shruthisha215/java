aad217e899f54c234167e73226c1b8bb69da4555
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 11:47:53 +0530

    continue statement

---
+++ contin.java
@@ -0,0 +1,15 @@
+public class contin
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+            if(i==8)
+            {
+                continue;
+            }
+        }
+    }
+
+}
