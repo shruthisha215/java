18a84100ea2081a66c3f04d637cba2e284b7d1c3
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-14 13:52:12 +0530

    arrays

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array
+{
+    public static void main(String args[])
+    {
+        String[] str={"rohini","priya","ram","sana"};
+        System.out.println("At Index [0]:"+str[0]);
+        System.out.println("At Index [1]:"+str[1]);
+        System.out.println("At Index [2]:"+str[2]);
+        System.out.println("At Index [3]:"+str[3]);
+    }
+}
