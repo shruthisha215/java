b4e9fc5a34b39a4a52b11b40c9d45aadb6e4ca14
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-15 11:58:04 +0530

    scanner

---
+++ obj.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class obj
+{
+public static void main(String args[])
+{
+ Scanner myobj = new Scanner(System.in);
+ System.out.println("enter the username:");
+ String username=myobj.next();
+ System.out.println("the username:" + username);
+}
+}
