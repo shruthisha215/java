81b1baa976cf574894bb42cee9c5d4bd3702dde8
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-15 10:26:31 +0530

    billing

---
+++ bill.java
@@ -0,0 +1,11 @@
+public class bill
+{
+    public static void main(String args[])
+    {
+        int[] bill_num={100,101,102,103,104,105};
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("the customer invoice number is:" +bill_num[i]);
+        }
+    }
+}
