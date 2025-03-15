f0621194e4c834ab27e5316e0f26aa6c5cbe9b3d
shruthishaml21@gmail.com <shruthishaml21@gmail.com>
2025-03-15 11:13:29 +0530

    coffee

---
+++ swit.java
@@ -0,0 +1,24 @@
+public class swit
+{
+    public static void main(String args[])
+    {
+        int choice=2;
+        switch(choice)
+        {
+            case 1:
+             System.out.println("coffee");
+             break;
+             case 2:
+             System.out.println("cold coffee");
+             break;
+             case 3:
+             System.out.println("chocolate coffee");
+             break;
+             case 4:
+                System.out.println("hot chocolate");
+                break;
+                default:
+                    System.out.println("invalid");
+        }
+    }
+}
